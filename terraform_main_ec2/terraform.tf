b8ca8751e6f376140143975921533fca89f639f1
CHERGUI-05 <riheb.chergui@univ-constantine2.dz>
2025-12-28 20:05:25 +0100

    Update terraform.tf

--- terraform_main_ec2/terraform.tf
+++ terraform_main_ec2/terraform.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "cher-gui-unique-bucket-12345"
+    bucket = "cher-gui-unique-bucket-67890"
     key    = "ec2/terraform.tfstate"
     region = "us-east-1"
   }
