d9115a9ebea455932b27b3b43b44de27b896e1f9
CHERGUI-05 <riheb.chergui@univ-constantine2.dz>
2025-12-28 19:39:09 +0100

    Update main.tf

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -20,10 +20,10 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "cher-gui-unique-bucket-67890"
+  bucket = "cher-gui-unique-bucket-12345"
 
   tags = {
-    Name        = "cher_gui_unique_bucket_67890"
+    Name        = "cher-gui-unique-bucket-12345"
     Environment = "dev"
   }
 }
