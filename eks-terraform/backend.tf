3e1def0a51422cf1451d2f53f914a53ac3dd0dca
CHERGUI-05 <riheb.chergui@univ-constantine2.dz>
2025-12-29 19:30:06 +0100

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -13,4 +13,18 @@ terraform {
   }
 
   required_version = ">= 1.6.3"
+}terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.25.0"
+    }
+  }
+
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+
+  required_version = ">= 1.6.3"
 }
+
