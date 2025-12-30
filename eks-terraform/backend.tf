a2778e9de2522a6eb2e4bb86a47127683f536630
CHERGUI-05 <riheb.chergui@univ-constantine2.dz>
2025-12-30 22:59:42 +0100

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,19 +1,6 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.25.0"
-    }
-  }
-
-  backend "s3" {
-    bucket = "aluruarumullaa1"
-    key    = "k8/terraform.tfstate"
-    region = "us-east-1"
-  }
-
   required_version = ">= 1.6.3"
-}terraform {
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -24,7 +11,5 @@ terraform {
   backend "local" {
     path = "terraform.tfstate"
   }
-
-  required_version = ">= 1.6.3"
 }
 
