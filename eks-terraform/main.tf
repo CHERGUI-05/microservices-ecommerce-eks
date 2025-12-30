3cb1630b9db7ea2b75065514d5699dc2788d2713
CHERGUI-05 <riheb.chergui@univ-constantine2.dz>
2025-12-31 00:51:13 +0100

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -32,7 +32,7 @@ data "aws_security_group" "selected" {
 }
 
 # ----------------------------
-# EKS Cluster (باستخدام Cluster موجود)
+# Existing EKS Cluster
 # ----------------------------
 data "aws_eks_cluster" "existing" {
   name = "my-test-eks"
@@ -43,45 +43,8 @@ data "aws_eks_cluster_auth" "existing" {
 }
 
 # ----------------------------
-# EKS Node Group (باستخدام Cluster موجود)
+# الملاحظات:
+# - لم نعد نحاول إنشاء Node Group
+# - لم نعد نحاول إنشاء IAM OIDC Provider
+# - Terraform الآن سيكتفي بالقراءة فقط من Cluster الموجود
 # ----------------------------
-resource "aws_eks_node_group" "node-grp" {
-  cluster_name    = data.aws_eks_cluster.existing.name
-  node_group_name = var.node_group_name
-  node_role_arn   = "arn:aws:iam::851725605085:role/ExistingWorkerRole"
-  subnet_ids      = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
-  capacity_type   = "ON_DEMAND"
-  disk_size       = 20
-  instance_types  = ["t2.large"]
-
-  labels = {
-    env = "dev"
-  }
-
-  tags = {
-    Name = "project-eks-node-group"
-  }
-
-  scaling_config {
-    desired_size = 3
-    max_size     = 10
-    min_size     = 2
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-}
-
-# ----------------------------
-# OIDC Provider for ServiceAccount IAM Roles
-# ----------------------------
-data "tls_certificate" "oidc_thumbprint" {
-  url = data.aws_eks_cluster.existing.identity[0].oidc[0].issuer
-}
-
-resource "aws_iam_openid_connect_provider" "eks_oidc" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint]
-  url             = data.aws_eks_cluster.existing.identity[0].oidc[0].issuer
-}
